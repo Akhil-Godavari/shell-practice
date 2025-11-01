78f94aaab44b81f7065a19f85862a9b76ac3b3ff
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-01 18:43:10 -0500

    adding new file 07-dynamic_variable.sh

--- 07-dynamic_variable.sh
+++ 07-dynamic_variable.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+Start_Time_exe=$(date +%c)
 Start_Time=$(date +%s)
+echo " Start Time of the script execution: $Start_Time_exe"
 sleep 10
+End_Time_exe=$(date +%c)
+echo " End Time of the script execution: $End_Time_exe"
 End_Time=$(date +%s)
 
 Total_Time=$(($End_Time-$Start_Time))

61af5059cb58d8d683255a1f711620600db2471c
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-01 17:19:59 -0500

    adding new file 04-variable.sh

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hi $Person2! How are you?"
+echo "$Person2:: Hello $Person1! I am Good"
+echo "$Person1:: What are you learning?"
+echo "$Person2:: I am learning shell scripting $Person1."

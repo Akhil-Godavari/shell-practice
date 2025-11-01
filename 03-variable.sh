d664e25360ead3f12e71bdf59632dea2f3a642e1
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-01 15:10:21 -0500

    adding new file : variable

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#No spaces to be added in between or beside equal
+Person1=Sony
+Person2=Akhil
+
+echo "$Person1:: Hi Suresh! How are you?"
+echo "$Person2:: Hello $Person1! I am Good"
+echo "$Person1:: What are you learning?"
+echo "$Person2:: I am learning shell scripting $Person1."

143bc5919ff65eab9ce63ef37499dadeac78b0a1
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-01 15:07:31 -0500

    adding new file : conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh! How are you?"
+echo "Suresh:: Hello Ramesh! I am Good"
+echo "Ramesh:: What are you learning?"
+echo "Suresh:: I am learning shell scripting Ramesh."

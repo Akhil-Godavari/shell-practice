f5f9d89d4c5d712e8a79aecf02cb4a949c6fc9db
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-06 20:00:10 -0600

    10-if-condition.sh program added

---
+++ 10-if-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " Enter the number: "
+read Number
+
+if [ ($($Number % 2)) -eq 0 ]; then
+    echo " Given number $Number is EVEN"
+else
+    echo " Given number $Number is ODD"
+fi
+
+

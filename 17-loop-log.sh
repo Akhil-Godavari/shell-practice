359ba4d5afe1554e69a8e7a1b12497211987a3c6
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-07 11:16:32 -0600

    17-loop-log.sh program added

---
+++ 17-loop-log.sh
@@ -0,0 +1,32 @@
+UserID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+
+Logs_Folder="/var/log/shell-script"
+Script_Name=$( echo $0 | cut -d "." -f1 )
+Log_File="$Logs_Folder/$Script_Name.log"
+
+mkdir -p $Logs_Folder
+echo " Script started executing at: $(date)"
+
+if [ $UserID -ne 0 ]; then
+    echo -e "$R ERROR :: Please run this script with ROOT Privilages $N" | tee -a $Log_File
+    exit 1 # failure code should be other than 0
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo -e " Installing $2 ..... $R FAILED $N" | tee -a $Log_File
+        exit 1
+    else
+        echo -e "Installing $2 .... $G SUCCESS $N" | tee -a $Log_File
+    fi
+}
+
+for package in $@
+do 
+    echo " Package is : $package "
+done

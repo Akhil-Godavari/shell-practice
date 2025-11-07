60fff317c3ae0c64002b49f2e91743a088166fef
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-06 19:48:05 -0600

    if-condition.sh program added

---
+++ 09-if-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Number=$1
+
+if[ $Number -lt 10 ]; then
+    echo "Given number $Number is less than 10"
+else
+    echo "Given number $Number is greater or equal to 10"
+fi 
+
+# -lt  :: less than
+# -gt  :: greater than
+# -eq  :: equal to
+# -ne  :: not equal

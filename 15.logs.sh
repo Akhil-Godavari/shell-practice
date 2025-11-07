90c59b83c8903eb7ce1701baa36411ae3bc442d5
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-07 10:54:53 -0600

    15-logs.sh program added

--- 15.logs.sh
+++ 15.logs.sh
@@ -13,16 +13,16 @@ mkdir -p $Logs_Folder
 echo " Script started executing at: $(date)"
 
 if [ $UserID -ne 0 ]; then
-    echo -e "$R ERROR :: Please run this script with ROOT Privilages $N"
+    echo -e "$R ERROR :: Please run this script with ROOT Privilages $N" | tee -a $Log_File
     exit 1 # failure code should be other than 0
 fi
 
 VALIDATE(){
     if [ $? -ne 0 ]; then
-        echo -e " Installing $2 ..... $R FAILED $N"
+        echo -e " Installing $2 ..... $R FAILED $N" | tee -a $Log_File
         exit 1
     else
-        echo -e "Installing $2 .... $G SUCCESS $N"
+        echo -e "Installing $2 .... $G SUCCESS $N" | tee -a $Log_File
     fi
 }
 dnf list installed mysql &>>$Log_File # Installed if it is not found
@@ -31,7 +31,7 @@ if [ $? -ne 0 ]; then
 dnf install mysql -y
 VALIDATE $1 "MYSQL"
 else
-    echo -e "MyQL already exist ... $Y SKIPPING INSTALLATION $N"
+    echo -e "MyQL already exist ... $Y SKIPPING INSTALLATION $N" | tee -a $Log_File
 fi
 
 dnf list installed nginx &>>$Log_File
@@ -40,7 +40,7 @@ if [ $? -ne 0 ]; then
 dnf install nginx -y
 VALIDATE $1 "NGINX"
 else
-    echo -e "NGINX already exist ... $Y SKIPPING INSTALLATION $N"
+    echo -e "NGINX already exist ... $Y SKIPPING INSTALLATION $N" | tee -a $Log_File
 fi
 
 dnf list installed python3 &>>$Log_File
@@ -49,7 +49,7 @@ if [ $? -ne 0 ]; then
 dnf install python3 -y
 VALIDATE $1 "Pythong3"
 else
-    echo -e "Pythong3 already exist ... $Y SKIPPING INSTALLATION $N"
+    echo -e "Pythong3 already exist ... $Y SKIPPING INSTALLATION $N" | tee -a $Log_File
 fi
 
 

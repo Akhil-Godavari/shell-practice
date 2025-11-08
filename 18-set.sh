40834b8d2fa0c8f203564d7fdf6361b27208c02f
Akhil-Godavari <akhilgodavari.21@gmail.com>
2025-11-07 20:43:49 -0600

    18-set.sh adding

--- 18-set.sh
+++ 18-set.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e 
 
+error(){
+    echo "There is an error"
+
+}
+
+trap error ERR
+
 echo "Hello..."
 echo "Before error..."
-ndkjfnajk;dnf
+ndkjfnajk;dnf # how shell understands there is an error and signal is ERR
 echo "After Error..."

683943ffcaab349f26a08fe7581b1c86a3d1612b
Akhil-Godavari <akhilgodavari.21@gmail.com>
2026-01-17 13:27:53 -0600

    adding new scripts 19,20 reading file by line

--- 20-while-loop.sh
+++ 20-while-loop.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-echo "Starting coutdown..."
+# echo "Starting coutdown..."
 
-while [ $count -gt 0 ]
-do
-    echo "Time left: $count"
-    sleep 1 #pauses for 1 second
-    count=$((count - 1))
-done
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left: $count"
+#     sleep 1 #pauses for 1 second
+#     count=$((count - 1))
+# done
 
-echo "Times UP"
+# echo "Times UP"
 
+while IFS= read -r line; do
+
+    echo "Processing line: $line"
+
+
+done < 16-forloop.sh

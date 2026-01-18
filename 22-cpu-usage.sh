9ab830c01e6e9c1df60975cc63cded8c00ca88db
Akhil-Godavari <akhilgodavari.21@gmail.com>
2026-01-18 15:03:17 -0600

    adding new scripts 22-csp-usage.sh

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -11,7 +11,7 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
 
-        MESSAGE="Hight Usage on $PARTITION: $USAGE"
+        MESSAGE+="Hight Usage on $PARTITION: $USAGE"
     fi
 
 done <<< $DISK_USAGE

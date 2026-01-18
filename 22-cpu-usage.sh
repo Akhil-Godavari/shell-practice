197e1aa90f65aae97320b76e4c057a5fe52b6518
Akhil-Godavari <akhilgodavari.21@gmail.com>
2026-01-18 15:02:09 -0600

    adding new scripts 22-csp-usage.sh

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -2,6 +2,8 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 # in prjects we keep i as 75
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+MESSAGE=""
 
 while IFS= read -r line;
 do  
@@ -9,7 +11,9 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
 
-        echo " Hight Usage on $PARTITION: $USAGE "
+        MESSAGE="Hight Usage on $PARTITION: $USAGE"
     fi
 
 done <<< $DISK_USAGE
+
+echo "Message Body: $MESSAGE"

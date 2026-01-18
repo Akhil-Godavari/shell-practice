5239cd9c0c2c78d7ca321ab0ded5df7b97c83c66
Akhil-Godavari <akhilgodavari.21@gmail.com>
2026-01-18 14:58:06 -0600

    adding new scripts 22-csp-usage.sh

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # in prjects we keep i as 75
 
 while IFS= read -r line;
 do  
-    echo "Line : $line"
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+
+        echo " Hight Usage on $PARTITION: $USAGE "
+    fi
 
 done <<< $DISK_USAGE
